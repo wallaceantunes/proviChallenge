2e6866b1be7b1c966668a5c9c332d3c7bafdce8b
Wallace Antunes <wallace.antunes@ubiqui.com.br>
2021-06-06 23:55:08 -0300

    make header

---
+++ src/components/utils/Header.js
@@ -0,0 +1,12 @@
+import '../../assets/scss/utils/header.scss';
+import Dropdown from './Dropdown'
+
+function Header() {
+    return (
+        <div className="header">
+            <Dropdown />
+        </div>
+    );
+}
+
+export default Header;
